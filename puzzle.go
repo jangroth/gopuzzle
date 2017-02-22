5c8da3bf131617f8bc56d8d6f1c7bed2cacc9913
Jan Groth <asiandub@gmx.de>
2017-02-23 07:08:36 +1100

    Add constructor for Piece

--- puzzle.go
+++ puzzle.go
@@ -19,33 +19,37 @@ type Puzzle struct {
 	maxX, maxY int
 }
 
+func (p *Piece) dump() {
+	dump(&p.piectrix)
+}
+
+func NewPiece(value int, points ...Point) *Piece {
+	maxX, maxY := 0, 0
+	for _, val := range points {
+		if val.x > maxX {
+			maxX = val.x
+		}
+		if val.y > maxY {
+			maxY = val.y
+		}
+	}
+	var matrix [][]int
+	for x := 0; x < maxX+1; x++ {
+		column := make([]int, maxY+1)
+		matrix = append(matrix, column)
+	}
+	for _, val := range points {
+		matrix[val.x][val.y] = value
+	}
+	return &Piece{piectrix: matrix}
+}
+
 func (p Point) String() string {
 	return fmt.Sprintf("(%d,%d)", p.x, p.y)
 }
 
 func (p *Puzzle) dump() {
-	for y := 0; y < p.maxY; y++ {
-		for x := 0; x < p.maxX; x++ {
-			fmt.Printf("%d ", p.matrix[x][y])
-		}
-		fmt.Println()
-	}
-	fmt.Printf("size: x:%d, y:%d\n", p.maxX, p.maxY)
-}
-
-func (p *Puzzle) nextFreeCell(pnt Point) Point {
-	fmt.Printf("\nnext free cell for: %s\n", pnt)
-	for y := pnt.y; y < p.maxY; y++ {
-		for x := 0; x < p.maxX; x++ {
-			fmt.Printf("%s:%d\n", Point{x, y}, p.matrix[x][y])
-			if (y == pnt.y && x > pnt.x) || y != pnt.y {
-				if p.matrix[x][y] == 0 {
-					return Point{x, y}
-				}
-			}
-		}
-	}
-	return Point{-1, -1}
+	dump(&p.matrix)
 }
 
 func NewPuzzle(maxX, maxY int) *Puzzle {
@@ -65,17 +69,30 @@ func NewPuzzle(maxX, maxY int) *Puzzle {
 	return &Puzzle{matrix: matrix, maxX: maxX, maxY: maxY}
 }
 
-func NewPiece(maxX, maxY, value int, points ...Point) *Piece {
-	var matrix [][]int
-	for x := 0; x < maxX; x++ {
-		column := make([]int, maxY)
-		matrix = append(matrix, column)
+func (p *Puzzle) nextFreeCell(pnt Point) Point {
+	fmt.Printf("\nnext free cell for: %s\n", pnt)
+	for y := pnt.y; y < p.maxY; y++ {
+		for x := 0; x < p.maxX; x++ {
+			fmt.Printf("%s:%d\n", Point{x, y}, p.matrix[x][y])
+			if (y == pnt.y && x > pnt.x) || y != pnt.y {
+				if p.matrix[x][y] == 0 {
+					return Point{x, y}
+				}
+			}
+		}
 	}
-	for _, val := range points {
-		matrix[val.x][val.y] = value
-	}
+	return Point{-1, -1}
+}
 
-	return &Piece{piectrix: matrix}
+func dump(matrix *[][]int) {
+	maxX, maxY := len(*matrix), len((*matrix)[0])
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			fmt.Printf("%d ", (*matrix)[x][y])
+		}
+		fmt.Println()
+	}
+	fmt.Printf("size: x:%d, y:%d\n", maxX, maxY)
 }
 
 func main() {
--- puzzle_test.go
+++ puzzle_test.go
@@ -5,8 +5,9 @@ import (
 )
 
 func TestNewPiece(t *testing.T) {
-	piece := NewPiece(2, 2, 2, Point{0, 0}, Point{1, 1})
-	if !(piece.piectrix[0][0] == 2 && piece.piectrix[0][1] == 0 && piece.piectrix[1][1] == 2 && piece.piectrix[1][0] == 0) {
+	piece := NewPiece(2, Point{0, 0}, Point{1, 1})
+	piece.dump()
+	if !(piece.piectrix[1][0] == 2 && piece.piectrix[0][1] == 0 && piece.piectrix[1][1] == 2 && piece.piectrix[1][0] == 0) {
 		t.Errorf("Testpiece doesn't look right %s", piece)
 	}
 }
